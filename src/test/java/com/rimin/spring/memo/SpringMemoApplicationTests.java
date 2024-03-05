bd77e046d48f990cb628a9f4ad002b5709b7166d
RIMIN <chris0540@naver.com>
2024-03-05 19:48:11 +0900

    [권민석]  - sts 초기 설정

---
+++ src/test/java/com/rimin/spring/memo/SpringMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimin.spring.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
