68739afad9acf2a7ebb4dcea9f70ab73538b8866
RIMIN <chris0540@naver.com>
2024-03-05 20:16:10 +0900

    [권민석]  - mapping parameter 수정

--- src/main/java/com/rimin/spring/memo/hello/HelloController.java
+++ src/main/java/com/rimin/spring/memo/hello/HelloController.java
@@ -1,13 +1,13 @@
 package com.rimin.spring.memo.hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	@ResponseBody
-	@RequestMapping("/hello")
+	@PostMapping("/hello")
 	public String helloWorld() {
 		return "Hello 123 World!!";
 	}
